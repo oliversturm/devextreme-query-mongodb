46b84398962ba7e939ca2930a0677670b4044bd2
Oliver Sturm <oliver@oliversturm.com>
2017-07-26 11:24:52 +0100

    implementation complete, but untested

--- options.js
+++ options.js
@@ -1,3 +1,6 @@
+import valueFixers from 'value-fixers';
+import parambulator from 'parambulator';
+
 function fixFilterAndSearch_(schema) {
   // currently only for int and float, since date and bool
   // are handled by heuristics
@@ -114,20 +117,113 @@ function fixFilterAndSearch(schema) {
   };
 }
 
+const sortOptionsChecker = parambulator({
+  required$: ['desc', 'selector'],
+  // isExpanded doesn't make any sense with sort, but the grid seems
+  // to include it occasionally - probably a bug
+  only$: ['desc', 'selector', 'isExpanded'],
+  desc: {
+    type$: 'boolean'
+  },
+  selector: {
+    type$: 'string'
+  }
+});
+
+const groupOptionsChecker = parambulator({
+  required$: ['selector'],
+  only$: ['desc', 'selector', 'isExpanded', 'groupInterval'],
+  desc: {
+    type$: 'boolean'
+  },
+  isExpanded: {
+    type$: 'boolean'
+  },
+  selector: {
+    type$: 'string'
+  },
+  groupInterval: {
+    type$: ['string', 'integer']
+    // unclear whether parambulator supports a spec that says "can be enum but also number"
+    //enum$: [ "year", "quarter", "month", "day", "dayOfWeek", "hour", "minute", "second" ] // and numbers?
+  }
+});
+
+const summaryOptionsChecker = parambulator({
+  required$: ['summaryType'],
+  only$: ['summaryType', 'selector'],
+  summaryType: {
+    enum$: ['sum', 'avg', 'min', 'max', 'count']
+  },
+  selector: {
+    type$: 'string'
+  }
+});
+
+function validateAll(list, checker, short = true) {
+  return list.reduce(
+    (r, v) => {
+      if (short && !r.valid) return r; // short circuiting
+      const newr = checker.validate(v);
+      if (newr) {
+        r.errors.push(newr);
+        r.valid = false;
+      }
+      return r;
+    },
+    { valid: true, errors: [] }
+  );
+}
+
+function parseOrFix(arg) {
+  return typeof arg === 'string'
+    ? JSON.parse(arg)
+    : valueFixers.fixObject(
+        arg,
+        valueFixers.defaultFixers.concat(valueFixers.fixBool)
+      );
+}
+
+function representsTrue(val) {
+  return val === true || val === 'true';
+}
+
 function wrapLoadOptions(lo) {
   return {
     loadOptions: lo
   };
 }
 
-function check(qry, oname, checker, wrapper = wrapLoadOptions) {
-  if (!qry || !qry[oname]) return {};
-  const checkResult = checker(qry[oname]);
-  return checkResult
-    ? wrapper(checkResult)
-    : {
-        errors: [`Invalid ${oname}: ${qry[oname]}`]
-      };
+function wrapProcessingOptions(po) {
+  return {
+    processingOptions: po
+  };
+}
+
+function check(
+  qry,
+  onames,
+  checker,
+  defaultValue = {},
+  wrapper = wrapLoadOptions
+) {
+  const options = typeof onames === 'string' ? [onames] : onames;
+  const allFound = qry && options.reduce((r, v) => r && qry[v], true);
+
+  if (!allFound) return defaultValue;
+  try {
+    const vals = options.map(o => qry[o]);
+    const checkResult = checker(...vals);
+    return checkResult
+      ? wrapper(checkResult)
+      : {
+          errors: options.map(o => `Invalid ${o}: ${qry[o]}`)
+        };
+  } catch (err) {
+    return {
+      errors: [err]
+    };
+  }
 }
 
 function takeOptions(qry) {
@@ -161,11 +257,178 @@ function totalCountOptions(qry) {
     qry,
     'requireTotalCount',
     requireTotalCount =>
-      requireTotalCount === true || requireTotalCount === 'true'
+      representsTrue(requireTotalCount)
         ? {
             requireTotalCount
           }
         : null
+  );
+}
+
+function sortOptions(qry) {
+  return check(qry, 'sort', sort => {
+    const sortOptions = parseOrFix(sort);
+    if (Array.isArray(sortOptions) && sortOptions.length > 0) {
+      const vr = validateAll(sortOptions, sortOptionsChecker);
+      if (vr.valid)
+        return {
+          sort: sortOptions
+        };
+      else
+        throw `Sort parameter validation errors: ${JSON.stringify(vr.errors)}`;
+    } else return null;
+  });
+}
+
+function groupOptions(qry) {
+  return check(qry, 'group', group => {
+    const groupOptions = parseOrFix(group);
+    if (Array.isArray(groupOptions)) {
+      if (groupOptions.length > 0) {
+        const vr = validateAll(groupOptions, groupOptionsChecker);
+        if (vr.valid)
+          return mergeResults([
+            {
+              group: groupOptions
+            },
+            check(qry, 'requireGroupCount', requireGroupCount => {
+              return representsTrue(requireGroupCount)
+                ? {
+                    requireGroupCount
+                  }
+                : null;
+            }),
+            check(qry, 'groupSummary', groupSummary => {
+              const gsOptions = parseOrFix(groupSummary);
+              if (Array.isArray(gsOptions)) {
+                if (gsOptions.length > 0) {
+                  const vr = validateAll(gsOptions, summaryOptionsChecker);
+                  if (vr.valid)
+                    return {
+                      groupSummary: gsOptions
+                    };
+                  else
+                    throw `Group summary parameter validation errors: ${JSON.stringify(vr.errors)}`;
+                } else return {}; // ignore empty array
+              } else return null;
+            })
+          ]);
+        else
+          throw `Group parameter validation errors: ${JSON.stringify(vr.errors)}`;
+      } else return {}; // ignore empty array
+    } else return null;
+  });
+}
+
+function totalSummaryOptions(qry) {
+  return check(qry, 'totalSummary', totalSummary => {
+    const tsOptions = parseOrFix(totalSummary);
+    if (Array.isArray(tsOptions)) {
+      if (tsOptions.length > 0) {
+        const vr = validateAll(tsOptions, summaryOptionsChecker);
+        if (vr.valid)
+          return {
+            totalSummary: tsOptions
+          };
+        else
+          throw `Total summary parameter validation errors: ${JSON.stringify(vr.errors)}`;
+      } else return {}; // ignore empty array
+    } else return null;
+  });
+}
+
+function filterOptions(qry) {
+  return check(qry, 'filter', filter => {
+    const filterOptions = parseOrFix(filter);
+    if (typeof filterOptions === 'string' || Array.isArray(filterOptions))
+      return {
+        filter: filterOptions
+      };
+    else return null;
+  });
+}
+
+function searchOptions(qry) {
+  return check(
+    qry,
+    ['searchExpr', 'searchOperation', 'searchValue'],
+    (se, so, sv) => {
+      if (typeof se === 'string' || Array.isArray(se))
+        return {
+          searchExpr: se,
+          searchOperation: so,
+          searchValue: sv
+        };
+      else return null;
+    }
+  );
+}
+
+function selectOptions(qry) {
+  return check(qry, 'select', select => {
+    const selectOptions = parseOrFix(select);
+    if (typeof selectOptions === 'string')
+      return {
+        select: [selectOptions]
+      };
+    else if (Array.isArray(selectOptions)) {
+      if (selectOptions.length > 0) {
+        if (selectOptions.reduce((r, v) => r && typeof v === 'string'))
+          return {
+            select: selectOptions
+          };
+        else
+          throw `Select array parameter has invalid content: ${JSON.stringify(selectOptions)}`;
+      } else return {}; // ignore empty array
+    } else return null;
+  });
+}
+
+function timezoneOptions(qry) {
+  return check(
+    qry,
+    'tzOffset',
+    tzOffset => ({
+      timezoneOffset: parseInt(tzOffset) || 0
+    }),
+    {
+      timezoneOffset: 0
+    },
+    wrapProcessingOptions
+  );
+}
+
+function summaryQueryLimitOptions(qry) {
+  return check(
+    qry,
+    'summaryQueryLimit',
+    sql => {
+      const psql = parseInt(sql);
+      return psql
+        ? {
+            summaryQueryLimit: psql
+          }
+        : {};
+    },
+    {},
+    wrapProcessingOptions
+  );
+}
+
+function mergeResults(results) {
+  return results.reduce(
+    (r, v) => ({
+      loadOptions: {
+        ...r.loadOptions,
+        ...v.loadOptions
+      },
+      processingOptions: {
+        ...r.processingOptions,
+        ...v.processingOptions
+      },
+      errors: [...r.errors, ...v.errors]
+    }),
+    {}
   );
 }
 
@@ -174,34 +437,21 @@ function getOptions(qry, schema) {
 
   const fixedQry = schema ? fixFilterAndSearch(schema)(qry) : qry;
 
-  return [
-    takeOptions,
-    skipOptions,
-    totalCountOptions,
-    sortOptions,
-    groupOptions,
-    totalSummaryOptions,
-    filterOptions,
-    searchOptions,
-    selectOptions,
-    timezoneOptions,
-    summaryQueryLimitOptions
-  ]
-    .map(f => f(fixedQry))
-    .reduce(
-      (r, v) => ({
-        loadOptions: {
-          ...r.loadOptions,
-          ...v.loadOptions
-        },
-        processingOptions: {
-          ...r.processingOptions,
-          ...v.processingOptions
-        },
-        errors: [...r.errors, ...v.errors]
-      }),
-      {}
-    );
+  return mergeResults(
+    [
+      takeOptions,
+      skipOptions,
+      totalCountOptions,
+      sortOptions,
+      groupOptions,
+      totalSummaryOptions,
+      filterOptions,
+      searchOptions,
+      selectOptions,
+      timezoneOptions,
+      summaryQueryLimitOptions
+    ].map(f => f(fixedQry))
+  );
 }
 
 module.exports = {
