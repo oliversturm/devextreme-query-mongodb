87aa9430d185037f96bacb88a5b1e4e7e732508e
Oliver Sturm <oliver@oliversturm.com>
2020-10-28 13:36:43 +0000

    version updates, tested

--- wallaby.js
+++ wallaby.js
@@ -1,10 +1,5 @@
 module.exports = () => ({
-  files: [
-    'src/*.js',
-    { pattern: 'src/options.test.js', ignore: true },
-    { pattern: 'src/pipelines.test.js', ignore: true },
-    { pattern: 'src/utils.test.js', ignore: true },
-  ],
+  files: ['src/*.js', { pattern: 'src/*.test.js', ignore: true }],
   // excluding index.test.js - these tests require running mongodb
   tests: ['src/options.test.js', 'src/pipelines.test.js', 'src/utils.test.js'],
 
